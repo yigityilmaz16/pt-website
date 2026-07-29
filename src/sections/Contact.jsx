4c0ec0010f32c9003077dc3c8410ea738f434230
yigityilmaz16 <yigityilmaz196345@gmail.com>
2026-07-29 14:50:43 +0300

    fix: change contact e-mail address and contact name

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -13,9 +13,9 @@ function Contact(){
                 <h4>İletişime Geç</h4>
             </div>
             <div className="contact-card">
-               <h4>Hasan Eren Serbest</h4>
+               <h4>Eren Serbest</h4>
                <p><a href="tel:+905300954266">+90 530 095 42 66</a></p> 
-               <p><a href="mailto:erenserbest16@gmail.com">erenserbest16@gmail.com</a></p>
+               <p><a href="mailto:serbesterenn@gmail.com">serbesterenn@gmail.com</a></p>
                <form onSubmit={handleSubmit}>
               <textarea id="feedback" name="feedback" rows="4" cols="50" placeholder="Sorularınız" value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
               <button type="submit">Soru Sor</button>
